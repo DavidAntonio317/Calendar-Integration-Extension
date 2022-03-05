e6db3dd2e1605eebc2c6ff86b0ef51f0aa59163d
Marshblocker <glmarinas@up.edu.ph>
2022-03-05 18:15:58 +0800

    Cycle between pomodoro session and break session

--- pomodoro.js
+++ pomodoro.js
@@ -1,4 +1,4 @@
-const SESSION_MINUTES = 1;
+const SESSION_MINUTES = 30;
 const BREAK_MINUTES = 5;
 
 class Pomodoro {
@@ -8,7 +8,8 @@ class Pomodoro {
         this.el = {
             minutes: root.querySelector('.pomodoro-timer-display-minutes'),
             seconds: root.querySelector('.pomodoro-timer-display-seconds'),
-            control: root.querySelector('.pomodoro-timer-control-btn')
+            control: root.querySelector('.pomodoro-timer-control-btn'),
+            status: root.querySelector('.pomodoro-status')
         };
 
         this.interval = null;
@@ -24,20 +25,38 @@ class Pomodoro {
     }
 
     startSession() {
+        this.el.status.textContent = 'In session...';
         this.el.control.textContent = 'Stop Session';
-        if (this.remainingSeconds === 0) return;
-
         this.interval = setInterval(() => {
             this.remainingSeconds--;
             this.updateTimerDisplay();
 
             if (this.remainingSeconds === 0) {
-                this.stopSession();
+                clearInterval(this.interval);
+                this.remainingSeconds = BREAK_MINUTES * 60;
+                this.updateTimerDisplay();
+                this.startBreak();
             }
         }, 1000);
     }
-
+    
+    startBreak() {
+        this.el.status.textContent = 'Taking a break...';
+        this.interval = setInterval(() => {
+            this.remainingSeconds--;
+            this.updateTimerDisplay();
+            
+            if (this.remainingSeconds === 0) {
+                clearInterval(this.interval);
+                this.remainingSeconds = SESSION_MINUTES * 60;
+                this.updateTimerDisplay();
+                this.startSession();
+            }
+        }, 1000);
+    }
+    
     stopSession() {
+        this.el.status.textContent = '';
         this.el.control.textContent = 'Start Session';
         clearInterval(this.interval);
         this.interval = null;
@@ -54,6 +73,7 @@ class Pomodoro {
 
     static getHTML() {
         return `
+            <div class="pomodoro-status"></div>
             <div class="pomodoro-timer-display">
                 <span class="pomodoro-timer-display-minutes">01</span>
                 <span class="pomodoro-timer-display-delim">:</span>
